46877a882b34bc194e3b0f2551de217663f3d33b
keshav singh <1816514912@kit.ac.in>
2022-12-14 22:24:03 +0530

    first angular commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,54 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'passwordGenr';
+  length = 0;
+  changeLetters = false;
+  changeNumbers = false;
+  changeSymbols = false;
+  Password: string = '';
+  onChangeLength(event: Event) {
+    const parsedvalue = parseInt((event.target as HTMLInputElement).value);
+    if (!isNaN(parsedvalue)) {
+      this.length = parsedvalue;
+    } else {
+      console.log('Enter only digit 0-9');
+    }
+  }
+  onChangeLetters() {
+    this.changeLetters = !this.changeLetters;
+  }
+  onChangeNumbers() {
+    this.changeNumbers = !this.changeNumbers;
+  }
+  onChangeSymbols() {
+    this.changeSymbols = !this.changeSymbols;
+  }
+  onClick() {
+    // console.log(
+    //   `Letter ${this.changeLetters}, Number ${this.changeNumbers}, Symbols ${this.changeSymbols}`
+    // );
+    var number = 1234567890;
+    var letter = 'abcdefghijklmnopqrstuvwxyz';
+    var symbol = '!@#$%^&*()';
+    let validChars = '';
+    if (this.changeLetters) {
+      validChars += letter;
+    }
+    if (this.changeNumbers) {
+      validChars += number;
+    }
+    if (this.changeSymbols) {
+      validChars += symbol;
+    }
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      var index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.Password = generatedPassword;
+  }
 }
